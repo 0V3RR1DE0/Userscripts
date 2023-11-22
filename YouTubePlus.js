9169071b31584f8d2fcf6e5c5c6695a235e660ce
Override <112493528+0V3RR1DE0@users.noreply.github.com>
2023-11-22 19:56:06 +0200

    Update YouTubePlus.js

--- YouTubePlus.js
+++ YouTubePlus.js
@@ -1,16 +1,23 @@
 // ==UserScript==
 // @name         YouTubePlus
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds lots of controls & features to YouTube videos.
 // @author       noEngineer
 // @match        https://www.youtube.com/*
 // @match        https://www.youtube-nocookie.com/*
+// @icon         https://example.com/icon.png  // Replace with the URL of your icon
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    // Function to create and show the settings menu
+    function showSettingsMenu() {
+        // Replace this with the actual settings menu implementation
+        alert('Settings menu will go here!');
+    }
 
     // Function to add the frame-by-frame controls
     function addFrameByFrameControls() {
@@ -43,9 +50,15 @@
             player.currentTime += 1 / player.playbackRate;
         });
 
+        // Create the "Settings" button
+        const settingsButton = document.createElement('button');
+        settingsButton.textContent = 'Settings';
+        settingsButton.addEventListener('click', showSettingsMenu);
+
         // Append buttons to the controls container
         controlsContainer.appendChild(prevFrameButton);
         controlsContainer.appendChild(nextFrameButton);
+        controlsContainer.appendChild(settingsButton);
 
         // Append the controls container to the video player parent
         player.parentElement.appendChild(controlsContainer);
@@ -60,4 +73,5 @@
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
+
 })();
